9da02ab3ac104c7448cfa21ea1dd4d561a273ddc
DarkAndBlue <13067258+DarkAndBlue@users.noreply.github.com>
2024-01-27 00:23:15 +0100

    Updated icon

--- src/client/java/de/darkandblue/keepthatmusic/mixin/MixinMusicTracker.java
+++ src/client/java/de/darkandblue/keepthatmusic/mixin/MixinMusicTracker.java
@@ -2,11 +2,7 @@ package de.darkandblue.keepthatmusic.mixin;
 
 import de.darkandblue.keepthatmusic.KeepThatMusic;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.sound.Channel;
-import net.minecraft.client.sound.MusicTracker;
-import net.minecraft.client.sound.SoundInstance;
-import net.minecraft.client.sound.SoundSystem;
-import net.minecraft.sound.MusicSound;
+import net.minecraft.client.sound.*;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -22,9 +18,9 @@ public class MixinMusicTracker {
 	@Shadow
 	private MinecraftClient client;
 	
-	SoundSystem lastSoundSystem;
-	Channel.SourceManager lastSourceManager;
-	SoundInstance lastSoundInstace;
+//	SoundSystem lastSoundSystem;
+//	Channel.SourceManager lastSourceManager;
+//	SoundInstance lastSoundInstace;
 	
 	/**
 	 * When a respawn packet comes in (respawn packet is also used for dimension switching like nether portals)
@@ -39,26 +35,27 @@ public class MixinMusicTracker {
 //		SoundSystem soundSystem = ((IMixinSoundManager) client.getSoundManager()).getSoundSystem();
 //		Channel.SourceManager sourceManager = ((IMixinSoundSystem) soundSystem).sourceManagerBySoundInstance(current);
 //		System.out.println(sourceManager.isStopped());
-//		
+//
 //		lastSoundSystem = soundSystem;
 //		lastSourceManager = sourceManager;
 //		lastSoundInstace = current;
 		
 		// TODO: save music on stopping (also in other mixin functions) and play it again when starting
 		
-//		ci.cancel();
+		ci.cancel();
 	}
 	
-	@Inject(method = "play", at = @At("HEAD"), cancellable = true)
-	public void play(MusicSound type, CallbackInfo ci) {
-		if (KeepThatMusic.DEBUG) {
-			System.out.println("MusicTracker.play() " + Math.random());
-		}
-		if(lastSoundInstace != null) {
-			current = lastSoundInstace;
-			ci.cancel();
-		}
-	}
+//	@Inject(method = "play", at = @At("HEAD"), cancellable = true)
+//	public void play(MusicSound type, CallbackInfo ci) {
+//		if (KeepThatMusic.DEBUG) {
+//			System.out.println("MusicTracker.play() " + Math.random());
+//		}
+//		if(lastSoundInstace != null) {
+//			this.current = lastSoundInstace;
+//			this.client.getSoundManager().play(this.current);
+//			ci.cancel();
+//		}
+//	}
 
 //	@Shadow
 //	private int timeUntilNextSong;
